512ba1fe4bac09507aff6099a7c05c3ab2bf0e12
David Yurek <david.a.yurek@gmail.com>
2013-11-23 14:28:00 -0500

    added c130-125easy.py and completes 130

---
+++ c128easy.py
@@ -0,0 +1,16 @@
+__author__ = 'davidyurek'
+__question__ = """
+    Given a well-formed (non-empty, fully valid) string of digits, let the integer N be the sum of digits. Then, given
+    this integer N, turn it into a string of digits. Repeat this process until you only have one digit left. Simple,
+    clean, and easy: focus on writing this as cleanly as possible in your preferred programming language. This challenge
+    is particularly easy, so don't worry about looking for crazy corner-cases or weird exceptions. This challenge is as
+    up-front as it gets :-) Good luck, have fun!
+
+    Input Description
+    On standard console input, you will be given a string of digits. This string will not be of zero-length and will be
+    guaranteed well-formed (will always have digits, and nothing else, in the string).
+
+    Output Description
+    You must take the given string, sum the digits, and then convert this sum to a string and print it out onto standard
+    console. Then, you must repeat this process again and again until you only have one digit left.
+    """
---
+++ c129easy.py
@@ -0,0 +1,13 @@
+__author__ = 'davidyurek'
+__question__ = """
+    This programming challenge is a classic[2] interview question for software engineers: given a string, find the
+    longest sub-string that contains, at most, two characters.
+
+    Input Description
+    Through standard console input, you will be given a string to search, which only contains lower-case alphabet
+    letters.
+
+    Output Description
+    Simply print the longest sub-string of the given string that contains, at most, two unique characters. If you find
+    multiple sub-strings that match the description, print the last sub-string (furthest to the right).
+    """
