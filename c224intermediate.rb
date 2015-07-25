8dedc52978a9585bf3c080999a8ac032450858cd
David Yurek <david.a.yurek@gmail.com>
2015-07-25 19:24:15 -0400

    finish the box detector with some sample resources

--- c224intermediate.rb
+++ c224intermediate.rb
@@ -2,39 +2,86 @@ __author__ = 'davidyurek'
 __question__ = 'Your program will be given an ASCII art chart showing boxes and lines. - and | characters indicate horizontal and vertical lines, respectively, while "+" characters show intersections.
 Your program should emit an integer, N, of how many unique four sided figures it found. Rectangles and squares both count.'
 
-def get_squares file_name
-  text = File.open(file_name).each do |line|
-    if line.index('+')
-      pluses = (0..line.length).find_all { |i| line[i, 1] == '+'}
-      potential_top_edge_pairs = top_edge_pairs pluses
-      for pair in potential_top_edge_pairs do
-        if /^[+](-+|\++)+[+]$/.match(line[pair[0]..pair[1]])
-          print true
-        end
-      end
-    else
-      puts "\n"
+class DetectFourSidedFigures
+  attr_reader :N, :filename
+
+  def initialize filename
+    @N = 0
+    @filename = filename
+    @current_line_index = 0
+    File.foreach(@filename).each do |line|
+      @current_line_index += 1
+      find_squares_per_line line
     end
   end
-end
 
-def top_edge_pairs array
-  pairs = []
-  if array.length < 2
-    pairs
-  elsif array.length == 2
-    pairs << array
-  else
-    for i in 2..array.length do
-      pairs << [array[0], array[i - 1]]
+  private
+
+  def find_squares_per_line line
+    if line.include?('+')
+      pluses = (0..line.length).find_all { |i| line[i, 1] == '+'}
+      potential_top_edge_pairs = top_edge_pairs_list pluses
+      valid_pairs = connected_top_edge_pairs potential_top_edge_pairs, line
+      for pair in valid_pairs do
+        read_file_for_completed_squares pair
+      end
     end
-    array.shift
-    pairs += top_edge_pairs(array)
+  end
+
+  def top_edge_pairs_list pluses
+    pairs = []
+    if pluses.length < 2
+      pairs
+    elsif pluses.length == 2
+      pairs << pluses
+    else
+      for i in 2..pluses.length do
+        pairs << [pluses[0], pluses[i - 1]]
+      end
+      pluses.shift
+      pairs += top_edge_pairs_list(pluses)
+    end
+  end
+
+  def connected_top_edge_pairs potential_top_edge_pairs, line
+    connected_pairs = []
+    for pair in potential_top_edge_pairs do
+      if valid_x_edge(line[pair[0]..pair[1]])
+        puts line[pair[0]..pair[1]]
+        connected_pairs << pair
+      end
+    end
+  end
+
+  def valid_x_edge string
+    string.each_byte do |ascii|
+      return false unless ascii.chr == '+' || ascii.chr == '-'
+    end
+    true
+  end
+
+  def read_file_for_completed_squares pair
+    count_down = @current_line_index
+    File.foreach(@filename).each do |line|
+      if count_down > 0
+        count_down -= 1
+        next
+      elsif line[pair[0]] == '+' &&
+            line[pair[1]] == '+' && 
+            valid_x_edge(line[pair[0]..pair[1]])
+        @N += 1
+      elsif (line[pair[0]] == '|' || line[pair[0]] == '+') &&
+            (line[pair[1]] == '|' || line[pair[1]] == '+')
+        next
+      else
+        break
+      end
+    end
   end
 end
 
 if __FILE__ == $0
-  file_name = './res/3977boxes.txt'
-  get_squares(file_name)
-  n = 0
+  filename = './res/3977boxes.txt'
+  dfsf = DetectFourSidedFigures.new(filename)
+  puts dfsf.filename + ': ' + dfsf.N.to_s
 end
