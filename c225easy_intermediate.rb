f6c581bc69f4232f47a8afda84b2537af3b17d7b
David Yurek <david.a.yurek@gmail.com>
2015-07-27 18:58:21 -0400

    challenge 225 easy; intermediate extension at a later time

---
+++ c225easy_intermediate.rb
@@ -0,0 +1,48 @@
+__author__ = 'davidyurek'
+__question = 'De-columnize a block of text, extracting the text, removing the features, and dehyphenating multiline words.'
+
+class Decolumnizer
+  attr_reader :paragraphs, :features_and_paragraphs
+
+  def initialize text
+    @paragraphs = get_paragraphs text
+    # @paragraphs = get_paragraphs(text)
+    # @features_and_paragraphs = get_features_and_paragraphs(text)
+  end
+
+  private
+
+  def get_paragraphs text
+    lines_array = text.split("\n")
+    lines_array = lines_array.map{ |line| remove_features line }
+    lines_array = lines_array.map(&:strip).map{ |line| dehyphenate line }
+    return lines_array.join('')
+  end
+
+  def get_features_and_paragraphs text
+    nil
+  end
+
+  def dehyphenate line
+    if line[-1] == '-'
+      return line[0..-2]
+    else
+      return line + ' '
+    end
+  end
+
+  def remove_features line
+    line.gsub(/\s*(\|.*(?=[|+])|\+-*(?=\+))+.\s*/, '')
+  end
+
+  def get_feature_strings text
+    nil
+  end
+end
+
+if __FILE__ == $0
+  dc =  Decolumnizer.new($stdin.read)
+  puts dc.paragraphs
+  # puts "\n"
+  # puts dc.features_and_paragraphs
+end
