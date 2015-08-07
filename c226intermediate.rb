d519c07523fce34c52cc35a222c4ead64c0bfb1f
David Yurek <david.a.yurek@gmail.com>
2015-08-07 09:00:56 -0400

    print board

--- c226intermediate.rb
+++ c226intermediate.rb
@@ -3,12 +3,30 @@ class ConnectFour
 
   def initialize
     @current_player = 'X'
+    @board_array = Array.new(7) { |a| Array.new(6) { |i| '.' } }
   end
 
   def play
+    print_board
+  end
+
+  private
+
+  def print_board
+    puts "\n"
+    puts '    a b c d e f g'
+    @board_array[0].length.downto(1) do |column|
+      print column.to_s + '   '
+      @board_array.length.times do |row|
+        print @board_array[row][column - 1] + ' '
+      end
+      puts "\n"
+    end
+    puts "\n"
   end
 end
 
 if __FILE__ == $0
-  ConnectFour.new
+  board = ConnectFour.new
+  board.play
 end
