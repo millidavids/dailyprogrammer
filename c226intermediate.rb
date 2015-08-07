50dc57ea80125f5134bea7532bfc7a9abad16fdb
David Yurek <david.a.yurek@gmail.com>
2015-08-07 16:10:25 -0400

    working on the play loop

--- c226intermediate.rb
+++ c226intermediate.rb
@@ -3,11 +3,29 @@ class ConnectFour
 
   def initialize
     @current_player = 'X'
-    @board_array = Array.new(7) { |a| Array.new(6) { |i| '.' } }
+    @board = Array.new(7) { |a| Array.new(6) { |i| '.' } }
+    @column_hash = { 'a': @board[0],
+                     'b': @board[1],
+                     'c': @board[2],
+                     'd': @board[3],
+                     'e': @board[4],
+                     'f': @board[5],
+                     'g': @board[6] }
+    @end = false
   end
 
   def play
-    print_board
+    while !@end do
+      print_board
+      valid_space = false
+      until valid_space do
+        puts @current_player + ', place piece: '
+        move = gets.chomp.downcase
+        if @column_hash.has_key?(move) && @board[move][6] == '.'
+        else
+        end
+      end
+    end
   end
 
   private
@@ -15,14 +33,17 @@ class ConnectFour
   def print_board
     puts "\n"
     puts '    a b c d e f g'
-    @board_array[0].length.downto(1) do |column|
+    @board[0].length.downto(1) do |column|
       print column.to_s + '   '
-      @board_array.length.times do |row|
-        print @board_array[row][column - 1] + ' '
+      @board.length.times do |row|
+        print @board[row][column - 1] + ' '
       end
       puts "\n"
     end
     puts "\n"
+  end
+
+  def place_piece index
   end
 end
 
