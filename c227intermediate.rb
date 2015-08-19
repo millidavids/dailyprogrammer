2274ac3471dee1d6eafa966d6ed6dceb86b4269c
David Yurek <david.a.yurek@gmail.com>
2015-08-19 18:08:50 -0400

    generating grid with random chain link placements

--- c227intermediate.rb
+++ c227intermediate.rb
@@ -1,7 +1,23 @@
 class ContiguousChain
-  def initialize grid_size link_percentage
+
+  def initialize grid_size, link_percentage
     @grid_size = grid_size
     @link_percentage = link_percentage
+    @grid = generate_grid
+    puts @grid
+  end
+
+  private
+
+  def generate_grid
+    grid = ''
+    @grid_size[1].times do
+      @grid_size[0].times do
+        grid << ((rand(100) < @link_percentage) ? 'x' : ' ')
+      end
+      grid << "\n"
+    end
+    return grid
   end
 end
 
@@ -19,17 +35,19 @@ end
 if __FILE__ == $0
   print 'Enter grid coordinate size pair, x first then y, space separated: '
   coordinate_string = gets.chomp.strip
-  grid_size = [0, 0]
+  grid_size = coordinate_string.strip.split(' ').map { |int| int.to_i }
   until /^\d+ \d+$/.match(coordinate_string) && elements_greater_than_zero(grid_size) do
     print 'Not valid coordinates, try again: '
     coordinate_string = gets.chomp
     grid_size = coordinate_string.strip.split(' ').map { |int| int.to_i }
   end
 
-  print "\n Enter chain link appearance percentage (number only): "
+  print "\nEnter chain link appearance percentage (number only): "
   link_percentage = gets.chomp.strip
   until /^\d+$/.match(link_percentage) && link_percentage.to_i <= 100
     print 'Not a valid percentage value, try again: '    
     link_percentage = gets.chomp.strip
   end
+
+  cc = ContiguousChain.new(grid_size, link_percentage.to_i)
 end
