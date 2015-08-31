09cbd66fe3a3adf6dbdd56a1b53869bc63a09a76
David Yurek <david.a.yurek@gmail.com>
2015-08-31 09:20:47 -0400

    calculate dottie number

--- c229easy.rb
+++ c229easy.rb
@@ -1,2 +1,14 @@
+def dottie_number input
+  dottie = Math.cos(input)
+  puts dottie
+  until dottie.round(8) == Math.cos(dottie).round(8)
+    dottie = Math.cos(dottie)
+    puts dottie
+  end
+end
+
 if __FILE__ == $0
+  print 'Input number: '
+  number = gets.chomp.to_i
+  dottie_number number
 end
