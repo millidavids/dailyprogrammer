29d9736ef836a0b9efefff790df61587269ebb23
David Yurek <david.a.yurek@gmail.com>
2015-09-05 19:49:59 -0400

    build initial hash

--- c229intermediate.rb
+++ c229intermediate.rb
@@ -1,13 +1,23 @@
 class ReverseFizzBuzz
-  def self.reversify input
+  def initialize input
     line_array = input.split("\n").map(&:strip)
-    letter_number_hash = Hash.new
+    @letter_number_hash = Hash.new
+    initial_hash(line_array)
+    
   end
 
   private
+
+  def initial_hash line_array
+    line_array.each do |line|
+      line.split('').each do |char|
+        @letter_number_hash[char] = 1 unless @letter_number_hash.has_key?(char)
+      end
+    end
+  end
 end
 
 if __FILE__ == $0
   input = $stdin.read
-  puts ReverseFizzBuzz.reversify(input)
+  rfb = ReverseFizzBuzz.new(input)
 end
