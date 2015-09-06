3ac330e72d08f4519f1e914fc344c4891d342298
David Yurek <david.a.yurek@gmail.com>
2015-09-06 09:21:40 -0400

    notice that if a more frequent element appears a number of times more than a larger element, that larger element is at least that number magnitude bigger

--- c229intermediate.rb
+++ c229intermediate.rb
@@ -1,27 +1,53 @@
 class ReverseFizzBuzz
   def initialize input
-    line_array = input.split("\n").map(&:strip)
+    @line_array = input.split("\n").map(&:strip)
     @frequencies = Hash.new
-    @letter_number_values_pairs = []
-    initial_data(line_array)
-    puts @letter_number_values_pairs.to_s
+    @pairs = []
+    @ratios = []
+    initial_data
+    build_ratios
+    puts @pairs.to_s
     puts @frequencies.to_s
+    puts @ratios.to_s
   end
 
   private
 
-  def initial_data line_array
-    line_array.each do |line|
+  def initial_data
+    @line_array.each do |line|
       line.split('').each do |char|
-        unless @letter_number_values_pairs.any? { |e| e[0] == char }
-          @letter_number_values_pairs << [char, 1]
+        unless @pairs.any? { |e| e[0] == char }
+          @pairs << [char, 1]
           @frequencies[char] = 1
         else
           @frequencies[char] += 1
         end 
       end
     end
-    @letter_number_values_pairs.sort_by! { |x| @frequencies[x[0]] }.reverse!
+    @pairs.sort_by! { |x| @frequencies[x[0]] }.reverse!
+  end
+
+  def build_ratios
+    @pairs[0..(@pairs.length - 2)].each_with_index do |first_pair, first_index|
+      line_ratios = []
+      @pairs[first_index+1..@pairs.length].each_with_index do |second_pair, second_index|
+        ratio_value = 0
+        @line_array.each do |line|
+          if line.include?(second_pair[0]) && line.include?(first_pair[0])
+            ratio_value += 1
+            break
+          elsif line.include?(second_pair[0])
+            break
+          elsif line.include?(first_pair[0])
+            ratio_value += 1
+          else
+            next
+          end
+        end
+        line_ratios << ratio_value
+      end
+      @ratios << line_ratios
+    end
   end
 end
 
