16467177962f937187a90ab4659d903f51f3c1c7
David Yurek <david.a.yurek@gmail.com>
2015-09-06 01:23:29 -0400

    sort the pairs by frequency

--- c229intermediate.rb
+++ c229intermediate.rb
@@ -1,11 +1,11 @@
 class ReverseFizzBuzz
   def initialize input
     line_array = input.split("\n").map(&:strip)
-    @letter_number_values = Hash.new
-    @letter_frequency = Hash.new
+    @frequencies = Hash.new
+    @letter_number_values_pairs = []
     initial_data(line_array)
-    puts @letter_frequency.to_s
-    puts @letter_number_values.to_s
+    puts @letter_number_values_pairs.to_s
+    puts @frequencies.to_s
   end
 
   private
@@ -13,13 +13,15 @@ class ReverseFizzBuzz
   def initial_data line_array
     line_array.each do |line|
       line.split('').each do |char|
-        unless @letter_number_values.has_key?(char)
-          @letter_number_values[char], @letter_frequency[char] = 1, 1
+        unless @letter_number_values_pairs.any? { |e| e[0] == char }
+          @letter_number_values_pairs << [char, 1]
+          @frequencies[char] = 1
         else
-          @letter_frequency[char] += 1
+          @frequencies[char] += 1
         end 
       end
     end
+    @letter_number_values_pairs.sort_by! { |x| @frequencies[x[0]] }.reverse!
   end
 end
 
