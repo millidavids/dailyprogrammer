78433b6f1e324eee1ebaa0034b0aef2a8222528d
David Yurek <david.a.yurek@gmail.com>
2015-09-05 20:13:52 -0400

    more frequent numbers have smaller values

--- c229intermediate.rb
+++ c229intermediate.rb
@@ -1,17 +1,23 @@
 class ReverseFizzBuzz
   def initialize input
     line_array = input.split("\n").map(&:strip)
-    @letter_number_hash = Hash.new
-    initial_hash(line_array)
-    
+    @letter_number_values = Hash.new
+    @letter_frequency = Hash.new
+    initial_data(line_array)
+    puts @letter_frequency.to_s
+    puts @letter_number_values.to_s
   end
 
   private
 
-  def initial_hash line_array
+  def initial_data line_array
     line_array.each do |line|
       line.split('').each do |char|
-        @letter_number_hash[char] = 1 unless @letter_number_hash.has_key?(char)
+        unless @letter_number_values.has_key?(char)
+          @letter_number_values[char], @letter_frequency[char] = 1, 1
+        else
+          @letter_frequency[char] += 1
+        end 
       end
     end
   end
