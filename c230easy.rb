9b84ee8c0cb9ccb0f059a6a29191aa350411eca5
David Yurek <david.a.yurek@gmail.com>
2015-10-01 07:49:11 -0400

    do not use jsonpath

--- c230easy.rb
+++ c230easy.rb
@@ -1,8 +1,10 @@
-require 'jsonpath'
+require 'JSON'
 
-def json_hunt
+def json_hunt string
+  json = JSON.parse(string)
 end
 
 if __FILE__ == $0
   input = $stdin.read
+  puts json_hunt(input)
 end
