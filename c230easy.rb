c7b6fa29e8413e8aa9c07d122c6462a858c5b9bc
David Yurek <david.a.yurek@gmail.com>
2015-10-01 07:57:25 -0400

    object oriented design

--- c230easy.rb
+++ c230easy.rb
@@ -1,10 +1,15 @@
 require 'JSON'
 
-def json_hunt string
-  json = JSON.parse(string)
+class JSONHunt
+  def initialize string
+    @json = JSON.parse(string)
+  end
+
+  def get_path
+  end
 end
 
 if __FILE__ == $0
   input = $stdin.read
-  puts json_hunt(input)
+  puts JSONHunt.new(input).get_path
 end
