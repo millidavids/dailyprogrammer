38fd5c7c1c75d4060c5bbffba7827f0ad6987505
David Yurek <david.a.yurek@gmail.com>
2015-10-02 16:29:06 -0400

    c230easy, pulled from examples

--- c230easy.rb
+++ c230easy.rb
@@ -5,11 +5,41 @@ class JSONHunt
     @json = JSON.parse(string)
   end
 
-  def get_path
+  def get_path item
+    path=[]
+    recursive_path_search item, path
+    return path.reverse
+  end
+
+  private
+
+  def recursive_path_search item, path, key='', obj=@json
+    case obj
+    when Hash
+      obj.each do |key, value|
+        path.unshift(key)
+        path.unshift(' > ')
+        recursive_path_search item, path, key, value
+        break if path.first == item
+        path.shift(2)
+      end
+    when Array
+      obj.each_with_index do |value, index|
+        path.unshift(index)
+        path.unshift(' > ')
+        recursive_path_search item, path, index, value
+        break if path.first == item
+        path.shift(2)
+      end
+    else
+      if obj == item
+        path.unshift(obj)
+      end
+    end
   end
 end
 
 if __FILE__ == $0
   input = $stdin.read
-  puts JSONHunt.new(input).get_path
+  puts JSONHunt.new(input).get_path('dailyprogrammer').to_s
 end
