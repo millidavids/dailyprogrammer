5ad8e0e1b2c48a63ad1ee0666d4be98bfcfe37f3
David Yurek <david.a.yurek@gmail.com>
2015-10-05 07:57:58 -0400

    begin the loop for automata

--- c231easy.rb
+++ c231easy.rb
@@ -1,6 +1,20 @@
+#!/usr/bin/ruby
+
 class CellularAutomata
+  attr_reader :initial_array
+
+  def initialize line
+    @initial_array = line.split('')
+  end
+
+  def output_automata_steps steps
+    automata_array = @initial_array.map{ |item| item == 1 ? 'x' : ' '}
+    steps.to_i.times do |step|
+    end
+  end
 end
 
 if __FILE__ == $0
-  input = gets.chomp
+  input = ARGV[0]
+  ca = CellularAutomata.new(input)
 end
