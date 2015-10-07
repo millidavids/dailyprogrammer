5132756cd86a271a29e70cffaeac3e69cb05bdc9
David Yurek <david.a.yurek@gmail.com>
2015-10-07 10:35:16 -0400

    finish c231easy

--- c231easy.rb
+++ c231easy.rb
@@ -4,26 +4,29 @@ class CellularAutomata
   attr_reader :initial_array
 
   def initialize line
-    @initial_array = line.split('')
+    @initial_array = line.split('').map(&:to_i)
   end
 
   def output_automata_steps steps
     automata_array = @initial_array
-    build_array = []
-    result_string = automata_array.map{ |item| item == 1 ? 'x' : ' ' }.join('')
-    steps.to_i.times do
-      automata_array.length.times do |index|
-        if index == 0 && automata_array[index + 1].to_i == 0
-          build_array << 0
-          result_string 
+    result_string = automata_array.map{ |item| item == 1 ? 'x' : ' '}.join + "\n"
+    steps.times do
+      automata_array = automata_array.map.with_index do |_, i|
+        if (i == 0 ? 0 : automata_array[i - 1]) != 
+           (i == automata_array.length - 1 ? 0 : automata_array[i + 1])
+          1
+        else
+          0
         end
       end
+      result_string << automata_array.map{ |item| item == 1 ? 'x' : ' '}.join + "\n"
     end
-    puts build_array.to_s
+    return result_string
   end
 end
 
 if __FILE__ == $0
   input = ARGV[0]
-  ca = CellularAutomata.new(input).output_automata_steps(25)
+  ca = CellularAutomata.new(input)
+  puts ca.output_automata_steps(10000)
 end
