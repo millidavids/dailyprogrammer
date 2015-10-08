44efb220df0cbf7d25325ea38aebfe7267f31785
David Yurek <david.a.yurek@gmail.com>
2015-10-08 07:58:11 -0400

    initialize class

--- c231intermediate.rb
+++ c231intermediate.rb
@@ -1,2 +1,13 @@
+class SetGame
+  def initialize input_array
+    @list = input_array
+  end
+
+  def get_sets
+  end
+end
+
 if __FILE__ == $0
+  input = $stdin.read.split("\n")
+  sg = SetGame.new(input)
 end
