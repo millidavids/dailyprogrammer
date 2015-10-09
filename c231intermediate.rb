b833c5f875e788bf8fac70604b6f41c43508cbbb
David Yurek <david.a.yurek@gmail.com>
2015-10-09 08:01:21 -0400

    figure out the recursive algorithm for iterating through a combination of sets of a certain size

--- c231intermediate.rb
+++ c231intermediate.rb
@@ -1,13 +1,28 @@
 class SetGame
-  def initialize input_array
+  attr_reader :list, :sets
+  def initialize input_array, sets_of
     @list = input_array
+    @sets = get_sets(input_array, 3)
   end
 
-  def get_sets
+  private
+
+  def get_sets array, sets_of
+    sets = []
+    if sets_of == 1
+      array.each_with_index do |i|
+      end
+    end
+  end
+
+  def all_equal
+  end
+
+  def all_different
   end
 end
 
 if __FILE__ == $0
   input = $stdin.read.split("\n")
-  sg = SetGame.new(input)
+  sg = SetGame.new(input, 3)
 end
