93b8ecbc7ab73582bfb3005e79610b46b24f3dbb
David Yurek <david.a.yurek@gmail.com>
2015-10-10 16:27:51 -0400

    finish c231intermediate

--- c231intermediate.rb
+++ c231intermediate.rb
@@ -1,28 +1,29 @@
 class SetGame
-  attr_reader :list, :sets
+  attr_reader :list, :sets, :set_size
+
   def initialize input_array, sets_of
     @list = input_array
-    @sets = get_sets(input_array, 3)
+    @set_size = sets_of
+    @sets = get_sets
   end
 
   private
 
-  def get_sets array, sets_of
-    sets = []
-    if sets_of == 1
-      array.each_with_index do |i|
-      end
+  def get_sets
+    return @list.combination(set_size).to_a.select{ |e| valid_set(e) }
+  end
+
+  def valid_set items
+    items[0].length.times do |i|
+      unique_letters = items.map{ |f| f[i] }.uniq.length
+      return false if unique_letters > 1 && unique_letters < @set_size
     end
-  end
-
-  def all_equal
-  end
-
-  def all_different
+    return true
   end
 end
 
 if __FILE__ == $0
   input = $stdin.read.split("\n")
   sg = SetGame.new(input, 3)
+  puts sg.sets.to_s
 end
