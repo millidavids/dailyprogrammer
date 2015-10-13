37cd4c9d04dae1f16f27b607e447843a1a941923
David Yurek <david.a.yurek@gmail.com>
2015-10-13 07:47:45 -0400

    initialize class object

--- c232intermediate.rb
+++ c232intermediate.rb
@@ -1,8 +1,11 @@
 class GrandmasHouse
   def initialize n_coordinates, coordinates_pairs
+    puts n_coordinates
+    puts coordinates_pairs.to_s
   end
 end
 
 if __FILE__ == $0
   input = $stdin.read.split("\n")
+  gh = GrandmasHouse.new(input[0], input[1..input.size])
 end
