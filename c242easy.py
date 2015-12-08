e8bfa99304cb0f0339c8a6c05744badd58cf2685
David Yurek <david.a.yurek@gmail.com>
2015-12-08 10:05:47 -0500

    finished c242easy

--- c242easy.py
+++ c242easy.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
 def weeksBeforeFed(people, fruits):
-  fruit_array = [fruits]
-  week = 1
+  fruits_array = [fruits]
+  week = 2
   while sum(fruits_array) < people:
     harvest = [1 for x in range(sum(fruits_array))]
-    
+    fruits_array = [x+1 for x in fruits_array] + harvest
+    week += 1
+  return week
 
 if __name__ == '__main__':
   inputs = raw_input('Enter number of people and number of fruit (space separated): ')
   people, fruits = [int(x) for x in inputs.split(' ')]
+  print weeksBeforeFed(people, fruits)
