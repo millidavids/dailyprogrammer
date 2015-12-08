32ca57ee4332248404da0bd211da4fa70ce5e3bc
David Yurek <david.a.yurek@gmail.com>
2015-12-08 07:51:47 -0500

    input for c242easy.py

--- c242easy.py
+++ c242easy.py
@@ -1,2 +1,5 @@
+#!/usr/bin/env python
+
 if __name__ == '__main__':
-  False
+  inputs = raw_input('Enter number of people and number of fruit (space separated): ')
+  people, fruits = [int(x) for x in inputs.split(' ')]
