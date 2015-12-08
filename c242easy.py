6ad39b2205578d971c40573dd4fba0dec12fe34a
David Yurek <david.a.yurek@gmail.com>
2015-12-08 07:59:26 -0500

    begin function

--- c242easy.py
+++ c242easy.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 
+def weeksBeforeFed(people, fruits):
+  fruit_array = [fruits]
+  week = 1
+  while sum(fruits_array) < people:
+    harvest = [1 for x in range(sum(fruits_array))]
+    
+
 if __name__ == '__main__':
   inputs = raw_input('Enter number of people and number of fruit (space separated): ')
   people, fruits = [int(x) for x in inputs.split(' ')]
