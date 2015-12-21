61520c37a01521598b5d355811db9562e725f81f
David Yurek <david.a.yurek@gmail.com>
2015-12-21 10:32:55 -0500

    finish c245easy

--- c245easy.py
+++ c245easy.py
@@ -4,8 +4,13 @@ import sys, re
 
 def dateDilemma(string):
   r = re.compile(r'\d+')
-  return r.findall(string)
+  split_string = ['0' + x if len(x) == 1 else x for x in r.findall(string)]
+  if len(split_string[0]) == 4:
+    return '-'.join(split_string)
+  else:
+    return '-'.join(['20' + split_string[2] if len(split_string[2]) == 2 else split_string[2]] + split_string[:2])
 
 if __name__ == '__main__':
   dates = sys.stdin.read().strip().split('\n')
-  print([dateDilemma(x) for x in dates], sep='\n')
+  print('\n')
+  print(*[dateDilemma(x) for x in dates], sep='\n')
